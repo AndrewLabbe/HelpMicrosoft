cb39e3254f8fc7e67be9621665c9ecd3bf2831dd
AndrewLabbe <ablabbe@quinnipiac.edu>
2022-04-18 19:44:29 -0400

    Added all logic to creating repo except for README and .gitignore

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,60 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//asks user for target repo's root folder
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter the path name to the target repo's root folder");
-		String repoPath = input.nextLine();
+		//String repoPath = input.nextLine();
 		
-		GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
+		//creates new gitSubproccessClient with target repo's root folder
+		GitSubprocessClient gitSubprocessClient = new GitSubprocessClient("C:/Users/labbe/Documents/Eclipse Workspace/CSC_111/Monster Project");
+		String gitInit = gitSubprocessClient.gitInit();
 		
+		//asks user for username and token
 		System.out.println("Please enter your username and token");
 		String user = input.nextLine();
 		String token = input.nextLine();
 		
+		//creates new gitHubApiClient with respective username and token 
 		GitHubApiClient gitHubApiClient = new GitHubApiClient(user, token);
 		
+		//asks user what they want repo name to be
 		System.out.println("Please enter desired Repo name");
 		String name = input.nextLine();
+		
+		//creates new repo with the above name parameter
 		RequestParams requestParams = new RequestParams();
 		requestParams.addParam("name", name);
-
+		
+		//asks if user wants descripton for repo
+		System.out.println("Would you like to set a description for the repo?");
+		String res = input.nextLine();
+		if(res.toLowerCase().equals("yes")) {
+			System.out.println("What would you like the description to be?");
+			String des = input.nextLine();
+			requestParams.addParam("description", des);
+		}
+		
+		//asks if user wants repo to be private or public
+		System.out.println("Would you like to make the repo private?");
+		res = input.nextLine();
+		if(res.toLowerCase().equals("yes")) {
+			requestParams.addParam("private", true);
+		}
+		
 		CreateRepoResponse createRepoResponse = gitHubApiClient.createRepo(requestParams);
+		
+		//gets repo's url
+		GetRepoInfoResponse repoInfo = gitHubApiClient.getRepoInfo(user, name);
+		String url = repoInfo.getJson().get("html_url").getAsString();
+		
+		//adds origin master, adds code, commits as "initial commit" and pushes it to master
+		String gitRemoteAdd = gitSubprocessClient.gitRemoteAdd("origin", url);
+		String gitAddAll = gitSubprocessClient.gitAddAll();
+		String commit = gitSubprocessClient.gitCommit("initial commit");
+		String push = gitSubprocessClient.gitPush("master");
+		
+		System.out.println("Success, your repo has been successfully created, url: " + url);
 	}
 	
 }
