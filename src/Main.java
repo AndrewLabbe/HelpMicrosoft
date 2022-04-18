da65b35afeac27e52ba537a42f29af7b9980ba44
AndrewLabbe <ablabbe@quinnipiac.edu>
2022-04-18 16:33:19 -0400

    Added main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import github.tools.client.GitHubApiClient;
+import github.tools.responseObjects.*;
+import git.tools.client.GitSubprocessClient;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
